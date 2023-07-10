7428a49817300a2add3decaf02c18455800ee986
Supreecha Jaijumpa <supreecha.j@pdkm.tech>
2023-07-10 11:36:26 +0700

    refactor: change func to arrow func

--- src/hooks/useCopyToClipboard.ts
+++ src/hooks/useCopyToClipboard.ts
@@ -1,9 +1,9 @@
 import { useState } from "react";
 
 type CopiedValue = string | null;
-type CopyFn = (text: string) => Promise<boolean>; // Return success
+type CopyFn = (text: string) => Promise<boolean>;
 
-export function useCopyToClipboard(): [CopiedValue, CopyFn] {
+export const useCopyToClipboard = (): [CopiedValue, CopyFn] => {
   const [copiedText, setCopiedText] = useState<CopiedValue>(null);
 
   const copy: CopyFn = async (text) => {
@@ -11,8 +11,6 @@ export function useCopyToClipboard(): [CopiedValue, CopyFn] {
       console.warn("Clipboard not supported");
       return false;
     }
-
-    // Try to save to clipboard then save it in the state if worked
     try {
       await navigator.clipboard.writeText(text);
       setCopiedText(text);
@@ -25,4 +23,4 @@ export function useCopyToClipboard(): [CopiedValue, CopyFn] {
   };
 
   return [copiedText, copy];
-}
+};
